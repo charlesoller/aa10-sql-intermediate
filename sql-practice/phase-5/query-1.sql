8ea5b8be3e3a7459c365fe6a0a53f4c873b36978
Max Fonda <mxwll155@gmail.com>
2024-01-19 20:19:56 -0500

    all phases complete

--- sql-practice/phase-5/query-1.sql
+++ sql-practice/phase-5/query-1.sql
@@ -1,2 +1,6 @@
 -- Find the first owner that has a last name containing the lowercase letter "r"
--- Your code here 
+-- Your code here
+SELECT *
+FROM owners
+WHERE owners.last_name LIKE '%r%'
+LIMIT 1 OFFSET 0;
--- sql-practice/phase-5/query-5.sql
+++ sql-practice/phase-5/query-5.sql
@@ -1,2 +1,8 @@
 -- Find names of the cats whose owners are both George Beatty and Melynda Abshire, or just George Beatty, or just Melynda Abshire
--- Your code here 
+-- Your code here
+SELECT cats.name
+FROM cat_owners
+JOIN owners ON (cat_owners.owner_id = owners.id)
+JOIN cats ON (cat_owners.cat_id = cats.id)
+WHERE owners.first_name IN ('George', 'Melynda')
+LIMIT 2 OFFSET 0;
